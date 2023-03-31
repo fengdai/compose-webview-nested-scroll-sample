ef7d3699b30c5a4040c8c764094ab1404c78f5e0
Feng Dai <toxic.dai@gmail.com>
2023-03-31 10:36:16 +0800

    tmp

--- sample/src/main/java/com/github/fengdai/compose/sample/MainActivity.kt
+++ sample/src/main/java/com/github/fengdai/compose/sample/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.github.fengdai.compose.sample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.animation.core.SpringSpec
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.OverscrollEffect
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsDraggedAsState
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.overscroll
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -19,18 +24,22 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource.Companion.Fling
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.node.Ref
+import androidx.compose.ui.unit.*
 import androidx.compose.ui.unit.Constraints.Companion.Infinity
-import androidx.compose.ui.unit.constrainWidth
-import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 import com.telefonica.nestedscrollwebview.NestedScrollWebView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import java.lang.Integer.max
+import kotlin.math.abs
 import kotlin.math.roundToInt
+import kotlin.math.sign
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,9 +49,9 @@ class MainActivity : ComponentActivity() {
                 topContent = {
                     Box(
                         modifier = Modifier
-                            .height(500.dp)
+                            .height(1000.dp)
                             .fillMaxWidth()
-                            .background(Brush.verticalGradient(listOf(Color.White, Color.Black)))
+                            .background(Brush.verticalGradient(listOf(Color.Black, Color.White)))
                     )
                 }
             )
@@ -55,42 +64,44 @@ fun rememberCommodityState(): CommodityState {
     return remember { CommodityState() }
 }
 
+@Stable
 class CommodityState {
-    var offsetY by mutableStateOf(0)
+    var scrollY by mutableStateOf(0)
         private set
-    var minOffsetY: Int
-        get() = _minOffsetYState.value
-        internal set(newMin) {
-            _minOffsetYState.value = newMin
-            if (offsetY < newMin) {
-                offsetY = newMin
+    var maxScrollY: Int
+        get() = _maxScrollYState.value
+        internal set(newMax) {
+            _maxScrollYState.value = newMax
+            if (scrollY > newMax) {
+                scrollY = newMax
             }
         }
-    private var _minOffsetYState = mutableStateOf(Int.MIN_VALUE)
+    private var _maxScrollYState = mutableStateOf(Int.MAX_VALUE)
     private var accumulator: Float = 0f
 
-    val reachMinOffsetY by derivedStateOf { offsetY <= minOffsetY }
-
-    val scrollableState = ScrollableState {
-        val absolute = (offsetY + it + accumulator)
-        val newValue = absolute.coerceIn(minOffsetY.toFloat(), 0f)
+    internal val scrollableState = ScrollableState {
+        val absolute = (scrollY + it + accumulator)
+        val newValue = absolute.coerceIn(0f, maxScrollY.toFloat())
         val changed = absolute != newValue
-        val consumed = newValue - offsetY
+        val consumed = newValue - scrollY
         val consumedInt = consumed.roundToInt()
-        offsetY += consumedInt
+        scrollY += consumedInt
         accumulator = consumed - consumedInt
 
         // Avoid floating-point rounding error
         if (changed) consumed else it
     }
 
-    val nestedScrollConnection = object : NestedScrollConnection {
+    internal lateinit var flingBehavior: FlingBehavior
+
+    internal val nestedScrollConnection = object : NestedScrollConnection {
         override fun onPreScroll(
             available: Offset,
             source: NestedScrollSource
         ): Offset {
-            return if (available.y < 0 || offsetY > minOffsetY) consume(available)
-            else super.onPreScroll(available, source)
+            return if (source != Fling && (available.y < 0 || scrollY < maxScrollY)) {
+                consume(available)
+            } else super.onPreScroll(available, source)
         }
 
         override fun onPostScroll(
@@ -98,17 +109,44 @@ class CommodityState {
             available: Offset,
             source: NestedScrollSource
         ): Offset {
-            return if (available.y > 0) consume(available)
+            return if (source != Fling && available.y > 0) consume(available)
             else super.onPostScroll(consumed, available, source)
+        }
+
+        override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+            if (available.y > 0) {
+                var remain = available.y
+                scrollableState.scroll {
+                    with(this) {
+                        with(flingBehavior) {
+                            remain = -performFling(-available.y)
+                        }
+                    }
+                }
+                return Velocity.Zero.copy(y = available.y - remain)
+            }
+            return Velocity.Zero
         }
     }
 
     private fun consume(available: Offset): Offset {
-        val consumedY = scrollableState.dispatchRawDelta(available.y)
+        val consumedY = -scrollableState.dispatchRawDelta(-available.y)
         return available.copy(y = consumedY)
     }
+
+    val canScrollForward by derivedStateOf { scrollY < maxScrollY }
+
+    suspend fun animateScrollTo(
+        scrollY: Float,
+        animationSpec: AnimationSpec<Float> = SpringSpec()
+    ) {
+        scrollableState.animateScrollBy(scrollY - this.scrollY, animationSpec)
+    }
+
+    suspend fun scrollTo(scrollY: Float): Float = scrollableState.scrollBy(scrollY - this.scrollY)
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Commodity(
     state: CommodityState = rememberCommodityState(),
@@ -116,10 +154,14 @@ fun Commodity(
 ) {
     val webViewRef = remember { Ref<NestedScrollWebView>() }
     val flingBehavior = ScrollableDefaults.flingBehavior()
+    SideEffect { state.flingBehavior = flingBehavior }
 
     val scrollableInteractionSource = remember { MutableInteractionSource() }
     val isDragged by scrollableInteractionSource.collectIsDraggedAsState()
-    fun isScrollableEnabled() = !state.reachMinOffsetY || isDragged
+    fun isScrollableEnabled() = state.canScrollForward || isDragged
+    val scope = rememberCoroutineScope()
+    // Create the overscroll controller
+    val overscroll = remember(scope) { OffsetOverscrollEffect(scope) }
     val outerNestedScrollConnection = remember {
         object : NestedScrollConnection {
             override fun onPreScroll(
@@ -156,6 +198,11 @@ fun Commodity(
                 }
                 return super.onPostScroll(consumed, available, source)
             }
+
+            override suspend fun onPostFling(consumed: Velocity, available: Velocity): Velocity {
+                overscroll.applyToFling(available) { Velocity.Zero }
+                return available
+            }
         }
     }
     Layout(
@@ -176,26 +223,32 @@ fun Commodity(
         },
         modifier = Modifier
             .fillMaxHeight()
+            .overscroll(overscroll)
             .nestedScroll(outerNestedScrollConnection)
             .scrollable(
                 state.scrollableState,
                 Orientation.Vertical,
                 enabled = isScrollableEnabled(),
-                flingBehavior = object : FlingBehavior {
-                    override suspend fun ScrollScope.performFling(initialVelocity: Float): Float {
-                        val remain = with(this) {
-                            with(flingBehavior) {
-                                performFling(initialVelocity)
+                reverseDirection = true,
+                flingBehavior = remember {
+                    object : FlingBehavior {
+                        override suspend fun ScrollScope.performFling(initialVelocity: Float): Float {
+                            val remain = with(this) {
+                                with(flingBehavior) {
+                                    performFling(initialVelocity)
+                                }
                             }
+                            if (remain > 0) {
+                                webViewRef.value?.fling(remain.roundToInt())
+                                return 0f
+                            }
+                            Log.d("Dai", "performFling: $initialVelocity -> $remain")
+                            return remain
                         }
-                        if (remain < 0) {
-                            webViewRef.value?.fling(-remain.roundToInt())
-                            return 0f
-                        }
-                        return remain
                     }
                 },
-                interactionSource = scrollableInteractionSource
+                interactionSource = scrollableInteractionSource,
+                overscrollEffect = overscroll,
             )
             .nestedScroll(state.nestedScrollConnection)
     ) { measurables, constraints ->
@@ -207,16 +260,82 @@ fun Commodity(
             measurables[1].measure(constraints.copy(minHeight = height, maxHeight = height))
         val width =
             constraints.constrainWidth(max(topContentPlaceable.width, bottomContentPlaceable.width))
-        state.minOffsetY = -topContentPlaceable.height
+        state.maxScrollY = topContentPlaceable.height
         layout(
             width = width,
             height = height
         ) {
-            topContentPlaceable.placeRelativeWithLayer(0, state.offsetY)
+            topContentPlaceable.placeRelativeWithLayer(0, -state.scrollY)
             bottomContentPlaceable.placeRelativeWithLayer(
                 0,
-                topContentPlaceable.height + state.offsetY
+                topContentPlaceable.height - state.scrollY
             )
         }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+class OffsetOverscrollEffect(val scope: CoroutineScope) : OverscrollEffect {
+    private val overscrollOffset = Animatable(0f)
+
+    override fun applyToScroll(
+        delta: Offset,
+        source: NestedScrollSource,
+        performScroll: (Offset) -> Offset
+    ): Offset {
+        // in pre scroll we relax the overscroll if needed
+        // relaxation: when we are in progress of the overscroll and user scrolls in the
+        // different direction = substract the overscroll first
+        val sameDirection = sign(delta.y) == sign(overscrollOffset.value)
+        val consumedByPreScroll = if (abs(overscrollOffset.value) > 0.5 && !sameDirection) {
+            val prevOverscrollValue = overscrollOffset.value
+            val newOverscrollValue = overscrollOffset.value + delta.y
+            if (sign(prevOverscrollValue) != sign(newOverscrollValue)) {
+                // sign changed, coerce to start scrolling and exit
+                scope.launch { overscrollOffset.snapTo(0f) }
+                Offset(x = 0f, y = delta.y + prevOverscrollValue)
+            } else {
+                scope.launch {
+                    overscrollOffset.snapTo(overscrollOffset.value + delta.y)
+                }
+                delta.copy(x = 0f)
+            }
+        } else {
+            Offset.Zero
+        }
+        val leftForScroll = delta - consumedByPreScroll
+        val consumedByScroll = performScroll(leftForScroll)
+        val overscrollDelta = leftForScroll - consumedByScroll
+        // if it is a drag, not a fling, add the delta left to our over scroll value
+        if (abs(overscrollDelta.y) > 0.5 && source == NestedScrollSource.Drag) {
+            scope.launch {
+                // multiply by 0.1 for the sake of parallax effect
+                overscrollOffset.snapTo(overscrollOffset.value + overscrollDelta.y * 0.1f)
+            }
+        }
+        return consumedByPreScroll + consumedByScroll
+    }
+
+    override suspend fun applyToFling(
+        velocity: Velocity,
+        performFling: suspend (Velocity) -> Velocity
+    ) {
+        Log.d("Dai", "applyToFling: $velocity")
+        val consumed = performFling(velocity)
+        // when the fling happens - we just gradually animate our overscroll to 0
+        val remaining = velocity - consumed
+        overscrollOffset.animateTo(
+            targetValue = 0f,
+            initialVelocity = remaining.y,
+            animationSpec = spring()
+        )
+    }
+
+    override val isInProgress: Boolean
+        get() = overscrollOffset.value != 0f
+
+    // as we're building an offset modifiers, let's offset of our value we calculated
+    override val effectModifier: Modifier = Modifier.offset {
+        IntOffset(x = 0, y = overscrollOffset.value.roundToInt())
     }
 }

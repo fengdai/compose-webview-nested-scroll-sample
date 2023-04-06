0d4ed298b0668659cdcc8b0ecf4a563c1440ff53
Feng Dai <toxic.dai@gmail.com>
2023-04-06 09:34:51 +0800

    Simplified

--- sample/src/main/java/com/github/fengdai/compose/sample/MainActivity.kt
+++ sample/src/main/java/com/github/fengdai/compose/sample/MainActivity.kt
@@ -5,8 +5,6 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.FlingBehavior
-import androidx.compose.foundation.gestures.ScrollScope
 import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -14,7 +12,6 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
@@ -22,6 +19,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.NestedScrollSource
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.Velocity
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -32,30 +30,46 @@ class MainActivity : ComponentActivity() {
             CompositionLocalProvider(LocalOverscrollConfiguration provides null) {
                 BoxWithConstraints {
                     val scrollState = rememberScrollState()
-                    val scrollFlingBehavior = ScrollableDefaults.flingBehavior()
                     val lazyListState = rememberLazyListState()
                     val lazyListFlingBehavior = ScrollableDefaults.flingBehavior()
                     Column(
                         Modifier
                             .fillMaxSize()
-                            .verticalScroll(scrollState, flingBehavior = remember {
-                                object : FlingBehavior {
-                                    override suspend fun ScrollScope.performFling(initialVelocity: Float): Float {
-                                        var remain = with(scrollFlingBehavior) {
-                                            performFling(initialVelocity)
+                            .nestedScroll(remember {
+                                object : NestedScrollConnection {
+                                    override fun onPreScroll(
+                                        available: Offset,
+                                        source: NestedScrollSource
+                                    ): Offset {
+                                        Log.d("Dai", "onPreScroll: $available")
+                                        if (available.y < 0 && scrollState.canScrollForward) {
+                                            val consumed =
+                                                -scrollState.dispatchRawDelta(-available.y)
+                                            return if (consumed == 0f) Offset.Zero
+                                            else Offset(0f, consumed)
                                         }
-                                        Log.d("Dai", "remain: $remain")
-                                        if (remain > 0) {
+                                        return super.onPreScroll(available, source)
+                                    }
+
+                                    override suspend fun onPostFling(
+                                        consumed: Velocity,
+                                        available: Velocity
+                                    ): Velocity {
+                                        if (available.y < 0 && lazyListState.canScrollForward) {
+                                            var remain = available.y
                                             lazyListState.scroll {
                                                 with(lazyListFlingBehavior) {
-                                                    remain = performFling(remain)
+                                                    remain = -performFling(-available.y)
                                                 }
                                             }
+                                            return if (remain == available.y) Velocity.Zero
+                                            else Velocity(0f, available.y - remain)
                                         }
-                                        return remain
+                                        return super.onPostFling(consumed, available)
                                     }
                                 }
                             })
+                            .verticalScroll(scrollState)
                     ) {
                         Column(
                             modifier = Modifier
@@ -64,31 +78,14 @@ class MainActivity : ComponentActivity() {
                         ) {
                             Box(
                                 modifier = Modifier
-                                    .height(500.dp)
+                                    .height(1000.dp)
                                     .fillMaxWidth()
                                     .background(
                                         Brush.verticalGradient(listOf(Color.White, Color.Black))
                                     )
                             )
-                            val coroutineScope = rememberCoroutineScope()
                             LazyColumn(
                                 modifier = Modifier
-                                    .nestedScroll(remember {
-                                        object : NestedScrollConnection {
-                                            override fun onPreScroll(
-                                                available: Offset,
-                                                source: NestedScrollSource
-                                            ): Offset {
-                                                if (available.y < 0 && scrollState.canScrollForward) {
-                                                    return Offset(
-                                                        0f,
-                                                        -scrollState.dispatchRawDelta(-available.y)
-                                                    )
-                                                }
-                                                return super.onPreScroll(available, source)
-                                            }
-                                        }
-                                    })
                                     .height(this@BoxWithConstraints.maxHeight)
                                     .fillMaxWidth(),
                                 state = lazyListState,
